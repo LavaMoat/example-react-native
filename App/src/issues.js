586add4fa5b55de07039b33229cc49ad2525d262
naugtur <naugtur@gmail.com>
2025-07-15 13:48:46 +0200

    override mistake demo fixed

--- App/src/issues.js
+++ App/src/issues.js
@@ -1,5 +1,5 @@
 
-const a = {};
+const a = function(){}
 a.field = 1;
-// cause an override mistake error
-a.toString = function nothing() {};
+// cause an override mistake error 
+a.apply = function nothing() {};
